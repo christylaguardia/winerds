300c1654ea990b6680fe042a94cad4c2f194e5ff
christylaguardia <christinelaguardia@gmail.com>
2018-01-29 23:42:14 -0800

    added hamburger open close

--- app/src/components/NavBar.js
+++ app/src/components/NavBar.js
@@ -1,49 +1,65 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-function NavBar({ user, logout }) {
-  return (
-    <nav className="navbar" aria-label="main navigation">
-      <div className="navbar-brand">
-        <div className="navbar-item">
+class NavBar extends React.Component {
 
-          <article className="media">
-            <figure className="media-left">
-              <p className="image is-32x32">
-                <img src={user.photoURL} alt={user.displayName} />
-              </p>
-            </figure>
-            <div className="media-content">
-              <div className="content">
-                <p>{user.displayName}</p>
-              </div>
-            </div>
-          </article>
-        </div>
+  state = {
+    open: false
+  }
 
-        <button className="navbar-burger burger" data-target="burger-options" onClick={() => { }}>
-          <span></span>
-          <span></span>
-          <span></span>
-        </button>
+  toggleMenu = () => this.setState(state => ({ open: !state.open }))
 
-      </div>
-
-      <div id="burger-options" className="navbar-menu">
-        <div className="navbar-start">
-          <Link className="navbar-item" to="/">New</Link>
-          <Link className="navbar-item" to="/notes">Tasting Notes</Link>
-        </div>
-
-        <div className="navbar-end">
+  render() {
+    const { user, logout } = this.props;
+    return (
+      <nav className="navbar" aria-label="main navigation">
+        <div className="navbar-brand">
           <div className="navbar-item">
-            <button className="button is-primary" onClick={logout}>Log Out</button>
+
+            <article className="media">
+              <figure className="media-left">
+                <p className="image is-32x32">
+                  <img src={user.photoURL} alt={user.displayName} />
+                </p>
+              </figure>
+              <div className="media-content">
+                <div className="content">
+                  <p>{user.displayName}</p>
+                </div>
+              </div>
+            </article>
+          </div>
+
+          <button
+            className={`navbar-burger burger ${this.state.open ? 'is-active' : ''}`}
+            data-target="burger-options" onClick={this.toggleMenu}>
+            <span></span>
+            <span></span>
+            <span></span>
+          </button>
+
+        </div>
+
+        <div
+          id="burger-options"
+          className={`navbar-menu ${this.state.open ? 'is-active' : ''}`}
+          onClick={this.toggleMenu}
+        >
+          <div className="navbar-start">
+            <Link className="navbar-item" to="/">New</Link>
+            <Link className="navbar-item" to="/notes">Tasting Notes</Link>
+          </div>
+
+          <div className="navbar-end">
+            <div className="navbar-item">
+              <button className="button is-primary" onClick={logout}>Log Out</button>
+            </div>
           </div>
         </div>
-      </div>
 
-    </nav>
-  );
+      </nav>
+    );
+  }
 }
 
 export default NavBar;
